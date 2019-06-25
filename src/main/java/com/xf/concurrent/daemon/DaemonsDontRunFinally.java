78465418a4799b7361bace262f2c8c19283445e5
zhanglei <zhanglei@jidao.ren>
2019-06-25 23:43:53 +0800

    springmvc

--- src/main/java/com/xf/concurrent/daemon/DaemonsDontRunFinally.java
+++ src/main/java/com/xf/concurrent/daemon/DaemonsDontRunFinally.java
@@ -24,7 +24,7 @@ class ADaemon implements Runnable {
 public class DaemonsDontRunFinally {
     public static void main(String[] args) {
         Thread t = new Thread(new ADaemon());
-//        t.setDaemon(true);
+        t.setDaemon(true);
         t.start();
     }
 }
